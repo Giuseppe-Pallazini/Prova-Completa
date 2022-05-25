255d0a92496db1ff7f5f8490fb7ffd89e3e961de
Giuseppe Pallazini <Pepeteste1883@gmail.com>
2022-05-25 11:37:16 -0300

    1.0

---
+++ src/pages/frequencia/index.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { useState } from "react"
+
+
+
+export default function Index() {
+    const [texto, setTexto] = useState('');
+    const [caractere, setCaractere] = useState('');
+    const [resposta, setResposta] = useState('');
+
+    async function verificarFreq() {
+        const resp = await axios.get(`http://localhost:5000/dia2/freqcaracter/${texto}/${caractere}`);
+        setResposta(resp.data.freq);
+    }
+
+    return (
+        <main>
+            <h1> Frequência </h1>
+            <div>
+                Texto: <input type='text' value={texto} onChange={e => setTexto(e.target.value)} />
+            </div> 
+            <div>
+                Caractere: <input type='text' value={caractere} onChange={e => setCaractere(e.target.value)} />
+            </div>
+            <div>
+                <button onClick={verificarFreq}> Verificar Frequencia</button>
+            </div>  
+            <div>
+                Frequencia: {resposta}
+            </div>
+
+
+        </main>
+    )
+}
---
+++ src/pages/maiorNumero/index.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { useState } from "react";
+export default function Index(){
+    const [numeros,setNumeros] = useState([]);
+    const [numero,setNumero] = useState(0);
+    const [resposta,setResposta] = useState('');
+
+    function adicionar(){
+        setNumeros([...numeros,numero])
+        setNumero('');
+    }
+
+
+    async function verificarMaior(){
+        const resp = await axios.post('http://localhost:5000/dia2/maiorNumero', numeros)
+        setResposta(resp.data.maior);
+    }
+
+    return(
+        <main> 
+        <h1> Maior Numero </h1>
+        <div> 
+            numero: <input type='text' value={numero} onChange={e => setNumero(Number(e.target.value))} /> 
+
+            <button onClick={adicionar}> Adicionar Número</button>
+        </div>
+
+        <div> 
+            {numeros.map(item=>
+                <div> {item} </div>
+            )}
+        </div>
+
+        <div> 
+           <button onClick={verificarMaior}> Verificar</button> Maior Numero é {resposta}
+        </div>
+        </main>
+    )
+
+}
